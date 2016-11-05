6964bb968a0b187668acb4d45278be8901e90088
embelsak <embelsak@foi.hr>
2016-11-05 09:06:10 +0100

    Napravljen GUI aplikacije
    Na glavnoj formi napravljena polja za unos kombinacije brojeva, prikaz
    dobitne kombinacije, te prikaz broja pogodaka nakon odigranog kola

--- LOTO/LOTO/Form1.cs
+++ LOTO/LOTO/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LOTO
 {
-    public partial class Form1 : Form
+    public partial class FrmLoto : Form
     {
-        public Form1()
+        public FrmLoto()
         {
             InitializeComponent();
         }
